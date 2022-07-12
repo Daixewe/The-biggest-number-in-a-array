334fdaebafd31b1d9550f23250e4deaf5d44a517
Daixewe <fuentesvd56@gmail.com>
2022-07-11 23:39:44 -0600

    The biggest number in a array done in c#

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Select length of the list");
+
+int size = int.Parse(Console.ReadLine());
+int[] list = new int [size];
+
+for (int i = 0;i < size; i++)
+{
+ Console.Write("Insert the number: ");
+ string input = Console.ReadLine() ?? "0";
+ list[i] = Int32.Parse(input);
+}
+  int maxElement = list[0];
+    for (int index = 1; index < list.Length; index++)
+    {
+        if (list[index] > maxElement)
+            maxElement = list[index];
+    }
+    Console.Write("The biggest number in the list is; "+maxElement);
